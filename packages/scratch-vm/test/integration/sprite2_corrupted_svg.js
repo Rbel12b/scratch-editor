0fb7789361fd39925483ba958bd8989e4f177210
Rbel12b <rbel12b@gmail.com>
2025-08-08 11:09:47 +0200

    test: fix test in scratch-vm; refactor tm2scratch extension to pass linting

--- packages/scratch-vm/test/integration/sprite2_corrupted_svg.js
+++ packages/scratch-vm/test/integration/sprite2_corrupted_svg.js
@@ -16,7 +16,8 @@ const FakeBitmapAdapter = require('../fixtures/fake-bitmap-adapter');
 const {extractAsset, readFileToBuffer} = require('../fixtures/readProjectFile');
 const VirtualMachine = require('../../src/index');
 const {serializeCostumes} = require('../../src/serialization/serialize-assets');
-const {sanitizeByteStream} = require('../../../scratch-svg-renderer/src/sanitize-svg');
+const {sanitizeSvg} = require('@scratch/scratch-svg-renderer');
+const {sanitizeByteStream} = sanitizeSvg;
 
 const projectUri = path.resolve(__dirname, '../fixtures/default.sb3');
 const project = readFileToBuffer(projectUri);
